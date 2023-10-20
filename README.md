f677c74af07f00f1365b301346314ec5eef1cacd
FengZhengHao <65449213+FengZhengHao-FZH@users.noreply.github.com>
2023-10-20 14:20:04 +0800

    初始化博客项目，添加文章

---
+++ README.md
@@ -0,0 +1,11 @@
+## YinLi - Site
+
+base on tailwind-css-starter-blog
+
+changes:
+- add hero images
+- close comment
+- close discuss on twitter
+- close view on github
+- close footer social 
+- close newsletter
---
+++ data/projectsData.ts
@@ -0,0 +1,24 @@
+const projectsData = [
+  {
+    title: 'ONE-DEV',
+    description: `一个用 Java 写的 Git 仓库管理平台。包含 Git 版本管理、持续集成、Issue 管理、代码搜索和审查等功能。`,
+    imgSrc: '/static/images/project/onedev.png',
+    href: 'https://github.com/theonedev/onedev',
+  },
+  {
+    title: 'Stirling-PDF',
+    description: `允许对 PDF 文件做各种操作的 Web 应用。这是一款功能强大、开箱即用的 PDF 工具，支持拆分/合并文件、添加/提取图片、压缩、加水印、添加/删除密码等功能，满足你对 PDF 文件的所有需求。`,
+    imgSrc: '/static/images/project/stirling-pdf.png',
+    href: 'https://github.com/Frooodle/Stirling-PDF',
+  },
+  {
+    title: 'A Search Engine',
+    description: `What if you could look up any information in the world? Webpages, images, videos
+    and more. Google has many features to help you find exactly what you're looking
+    for.`,
+    imgSrc: '/static/images/project/google.png',
+    href: 'https://www.google.com',
+  },
+]
+
+export default projectsData
