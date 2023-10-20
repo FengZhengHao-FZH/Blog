0fcd247210650025fbd7ceceb66fbc6021542be2
FengZhengHao <65449213+FengZhengHao-FZH@users.noreply.github.com>
2023-10-20 15:16:11 +0800

    无法修复tag-data.json移动路径导致的错误，将其重新放回了app目录下

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -1,7 +1,7 @@
 import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import { slug } from 'github-slugger'
-import tagData from '@/data/tag-data.json'
+import tagData from '../tag-data.json'
 import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({ title: 'Tags', description: 'Things I blog about' })
